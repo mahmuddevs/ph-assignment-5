7b8c561506360a469193d0fc0fba0f1ec57521be
Mahmud Rashid <srabon5704@gmail.com>
2024-09-26 01:27:01 +0600

    trying to fix redirect issue in github pages

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ window.addEventListener("scroll", function () {
 const homeBtn = document.getElementById('home')
 if (homeBtn) {
     homeBtn.addEventListener('click', function () {
-        window.location.href = '/';
+        window.location.href = 'index.html';
     })
 }
 document.getElementById('blog').addEventListener('click', function () {
